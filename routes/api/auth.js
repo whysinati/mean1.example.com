af55173c8ef1f52fd2efcce20a09c64a77cd87e9
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-22 16:01:53 -0500

    add an endpoint for user authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -34,4 +34,31 @@ router.post('/register', function(req,res,next){
 
 });
 
-module.exports = router;
+router.post('/login', function(req, res, next) {
+    //
+    passport.authenticate('local', function(err, user, info) {
+  
+      if (err) { 
+        return res.json({success:false, error: err});
+      }
+  
+      if (!user) {
+        return res.json({success:false, error: info.message });
+      }
+  
+      req.logIn(user, function(err) {
+  
+        if (err) { 
+          return res.json({success:false, error: err });
+        }
+  
+        //we will use a console.log() to test the session data
+        console.log(req.session);
+  
+        return res.json({success:true, user: user });
+  
+      });
+    })(req, res, next);
+  });
+
+  module.exports = router;
