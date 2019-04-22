6115cd6f0fd55d205775770bec09347514fb382b
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-22 15:28:01 -0500

    add configuration objects for sessions and cookies

--- config.dev.js
+++ config.dev.js
@@ -1,6 +1,5 @@
 var config = {};
 config.mongodb = 'mongodb://localhost/mean-cms';
-module.exports = config;
 
 //Session configuration object
 config.session = {};
@@ -15,3 +14,5 @@ config.session.secret = '7j&1tH!cr4F*1U';
 
 //Define the domain for which this cookie is to be set
 config.cookie.domain = 'localhost';
+
+module.exports = config;
