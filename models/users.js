a8a2ba9a8d8c78af6f8b60856f2d35f7c19cd22e
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-22 15:45:19 -0500

    add passport to the users model

--- models/users.js
+++ models/users.js
@@ -47,9 +47,11 @@ var Users = new Schema({
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
 
+Users.plugin(passportLocalMongoose);
+
 Users.pre('save', function(next){
   this.modified = new Date().toISOString();
   next();
 });
-Users.plugin(passportLocalMongoose);
+
 module.exports  = mongoose.model('Users', Users);
