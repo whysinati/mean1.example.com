14c2f7eab6dedf5be92c69fb7bebca639912bbef
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-22 14:42:23 -0500

    auto-update the modified date when a document is saved

--- models/users.js
+++ models/users.js
@@ -33,4 +33,9 @@ var Users = new Schema({
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
 
+Users.pre('save', function(next){
+  this.modified = new Date().toISOString();
+  next();
+});
+
 module.exports  = mongoose.model('Users', Users);
