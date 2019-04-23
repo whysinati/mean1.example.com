4123b72387953005a91ac4f574ffe1d8a2e1d333
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-23 13:20:31 -0500

    added error/sucess handling to the form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -49,6 +49,11 @@ var authApp = (function() {
         xhr.onload = function(){
           let data = JSON.parse(xhr.response);
           console.log(data);
+          if(data.success===true){
+            window.location.href = '/';
+          }else{
+            document.getElementById('formMsg').style.display='block';
+          }
         }
       });
     }  
