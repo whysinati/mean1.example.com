7826d74164da08dc22704d3b71e0a1fec7782792
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-24 09:29:55 -0500

    added whitelist and fixed login/logout errors

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,18 +11,18 @@ router.post('/register', function(req,res,next){
     email: data.email,
     first_name: data.first_name,
     last_name: data.last_name
-  }), 
-  data.password, 
+  }),
+  data.password,
   function(err, user){
 
     if(err){
 
       return res.json({
-        success: false, 
-        user: req.body, 
+        success: false,
+        user: req.body,
         errors: err
       });
-      
+
     }
 
     return res.json({
@@ -35,42 +35,42 @@ router.post('/register', function(req,res,next){
 });
 
 router.post('/login', function(req, res, next) {
-    //
-    passport.authenticate('local', function(err, user, info) {
-  
-      if (err) { 
-        return res.json({success:false, error: err});
+  //
+  passport.authenticate('local', function(err, user, info) {
+
+    if (err) {
+      return res.json({success:false, error: err});
+    }
+
+    if (!user) {
+      return res.json({success:false, error: info.message });
+    }
+
+    req.logIn(user, function(err) {
+
+      if (err) {
+        return res.json({success:false, error: err });
       }
-  
-      if (!user) {
-        return res.json({success:false, error: info.message });
-      }
-  
-      req.logIn(user, function(err) {
-  
-        if (err) { 
-          return res.json({success:false, error: err });
+
+      //we will use a console.log() to test the session data
+      console.log(req.session);
+
+      return res.json({success:true, user: user });
+
+      router.delete('/logout', function(req, res){
+        req.logout();
+        if(!req.session.passport.user){
+          return res.json({success: 'true'});
+        }else{
+          return res.json({success: 'false'});
         }
-  
-        //we will use a console.log() to test the session data
-        console.log(req.session);
-  
-        return res.json({success:true, user: user });
-        router.delete('/logout', function(req, res){
-          req.logout();
-          if(!req.session.passport.user){
-            return res.json({success: 'true'});
-          }else{
-            return res.json({success: 'false'});
-          }
-        });
-
       });
-    })(req, res, next);
-  });
+    });
+  })(req, res, next);
+});
 
-  router.get('/logout', function(req, res){
-    req.logout();
-  });
+router.get('/logout', function(req, res){
+  req.logout();
+});
 
-  module.exports = router;
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   console.log(req.session);
-  res.render('index', { title: 'Express' });
+  res.send('Welcome to MicroTrain1903');
 });
 router.get('/logout', function(req, res){
   console.log(req.session);
   req.logout();
   console.log(req.session);
 });
+
 module.exports = router;
